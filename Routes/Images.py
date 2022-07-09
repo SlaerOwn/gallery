76ce178fe2c9ca2a17110d156297d599ae08c064
chelavik <106871646+chelavik@users.noreply.github.com>
2022-07-10 03:03:52 +0400

    Update Images.py

--- Routes/Images.py
+++ Routes/Images.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, HTTPException
 from Database.Database import *
 from Utils.Hasher import HasherClass
-from Models.api import TokenResponse
+
 
 router = APIRouter()
+
 
 Hasher = HasherClass()
 Database = DatabaseClass()
@@ -13,10 +14,10 @@ Database = DatabaseClass()
 async def create_image(login: str, token: str, image: str, description: str):
     try:
         if Database.is_authorized(login, token):
-            Database.create_image(image, description)
+            Database.add_photo(image, description)
             return HTTPException(
                 status_code=200,
-                detail= 'OK')
+                detail='OK')
 
     except NoPermissionError:
         return HTTPException(status_code=403,
@@ -29,14 +30,14 @@ async def create_image(login: str, token: str, image: str, description: str):
 
 @router.get('/images')
 async def get_all_images():
-    return Database.get_images()
+    return Database.get_photos()
 
 
 @router.delete('/images/{image_id}')
 async def delete_image(login: str, token: str, ID: str):
     try:
         if Database.is_authorized(login, token):
-            Database.delete_image(ID)
+            Database.delete_photo(ID)
             return HTTPException(status_code=200, detail='OK')
 
     except NoPermissionError:
